b8c0e1d9e1953cd75668a3242f015901b6643976
Taka <takahito.uchino.1218@gmail.com>
2024-04-28 15:43:49 +0900

    [add]React Router動作確認
    React Routerが動作することを確認しました。
    作成したページごとにpagesディレクトリに格納してください。

---
+++ frontend/app/src/pages/home.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <>
+      <h2 className="text-xl">Home</h2>
+      <Link to='/login'>Login</Link>
+    </>
+  )
+}
---
+++ frontend/app/src/pages/login.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  return (
+    <>
+      <h2 className="text-xl">Login</h2>
+      <Link to='/'>Home</Link>
+    </>
+  )
+}
