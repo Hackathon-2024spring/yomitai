70ac1edd25979ce7a276eba11a679d26e3dab0a9
Taka <takahito.uchino.1218@gmail.com>
2024-04-29 17:41:10 +0900

    [add]サインアップページ追加
    スタイルの当たっていないサインアップページのUIを作りました。

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import Home from './pages/home'
 import Login from './pages/login'
+import Signup from './pages/signup'
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
   }
 ])
 
---
+++ frontend/app/src/pages/signup.tsx
@@ -0,0 +1,39 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Link } from "react-router-dom";
+type SignupForm = {
+  username: string
+  email: string
+  password: string
+}
+
+export default function Signup() {
+  const { register, handleSubmit, formState: { errors } } = useForm<SignupForm>();
+
+  const onSubmit: SubmitHandler<SignupForm> = (data) => console.log(data);
+
+  return (
+    <div>
+      <h1 className="text-xl">Signup</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input id="username" className="border" {...register("username", { required: true })} placeholder="Username" />
+          {errors.username && <span>ユーザー名が必要です</span>}
+        </div>
+        <div>
+          <input id="email" className="border" {...register("email", { required: true })} placeholder="Email" />
+          {errors.email && <span>メールアドレスが必要です</span>}
+        </div>
+        <div>
+          <input id="password" className="border" {...register("password", { required: true })} type="password" placeholder="Password" />
+          {errors.password && <span>パスワードが必要です</span>}
+        </div>
+        <div>
+          <input id="password" className="border" {...register("password", { required: true })} type="password" placeholder="Password" />
+          {errors.password && <span>パスワードが必要です</span>}
+        </div>
+        <button type="submit">サインアップ</button>
+      </form>
+      <Link to="/login">ログインはこちら</Link>
+    </div>
+  )
+}
