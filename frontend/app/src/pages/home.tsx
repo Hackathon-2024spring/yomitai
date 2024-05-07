4abf21e00d1efc00cf0675d62e0bb9fe5a1c30e8
kunihiko <127746548+kunipico@users.noreply.github.com>
2024-05-07 23:43:27 +0900

    [ADD]カレンダーコンポーネント追加

--- frontend/app/src/pages/home.tsx
+++ frontend/app/src/pages/home.tsx
@@ -1,10 +1,13 @@
 import Header from "../components/Header";
+import { CalendarComponent } from "../components/calendarComponent";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div className="bg-yellow-50 text-xl"></div>
+      <div className="h-screen w-screen bg-yellow-50 text-xl">
+        <CalendarComponent />
+      </div>
     </>
   );
 }
