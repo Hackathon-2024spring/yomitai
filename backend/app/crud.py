88117a5c1dda1842ac06dd54a18152dfecf3edda
羽田　有希 <naoki@hatayukinoMacBook-Pro.local>
2024-05-11 01:03:34 +0900

    update_crud.py

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -2,11 +2,10 @@ from sqlalchemy.orm import Session
 from datetime import date, datetime
 from sqlalchemy import func
 from . import models, schemas
-from .schemas import Book
+from .schemas import Book, MyBook
 from passlib.context import CryptContext
 from .auth import hash_password  # auth.pyからハッシュ化関数をインポート
-from fastapi import HTTPException, status
-from .session_store import sessions
+from fastapi import HTTPException
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
@@ -22,6 +21,13 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
+
+
+# パスワードのハッシュ化設定
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# def hash_password(password: str) -> str:
+#     return pwd_context.hash(password)
 
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = hash_password(user.password)
@@ -59,16 +65,44 @@ def create_user(db: Session, user: schemas.UserCreate):
 def get_book_by_isbn(db: Session, isbn: str):
     return db.query(models.Book).filter(models.Book.isbn_code == isbn).first()
 
-# booksテーブルにレコードを追加
-def create_book(db: Session, book: Book):
+
+# my_booksテーブルにレコードを追加（自動登録）
+def create_my_book(db: Session, book: MyBook, user_id: int,genre_id: int, book_id: int):
+    # 新しいMy_bookオブジェクトの作成
+    db_my_book = models.My_book(
+        user_id=user_id,
+        book_id=book_id,
+        genre_id=genre_id,
+        title=book.title,
+        author=book.author,
+        publisher=book.publisher,
+        total_page=book.total_page,
+        image=book.image,
+        start_date=book.start_date,
+        planned_end_date=book.planned_end_date,
+        end_date=None
+     )
+    # 新しい書籍をデータベースセッションに追加
+    db.add(db_my_book)
+    # 変更をコミットしてデータベースに保存
+    db.commit()
+    # 新しい書籍情報をリフレッシュして、IDなどの自動生成フィールドを含める
+    db.refresh(db_my_book)
+    # 新しい書籍オブジェクトを返す
+    return db_my_book
+
+
+# booksテーブルとmy_booksテーブルにレコードを追加（自動登録）
+def create_book_and_my_book(db: Session, book: Book,user_id: int,genre_id: int):
     # 新しいBookオブジェクトの作成
     db_book = models.Book(
         title=book.title,
         author=book.author,
         publisher=book.publisher,
         total_page=book.total_page,
-        isbn_code=book.isbn,
+        isbn_code=book.isbn_code,
         image=book.image,
+        genre_id=genre_id
      )
     # 新しい書籍をデータベースセッションに追加
     db.add(db_book)
@@ -76,9 +110,57 @@ def create_book(db: Session, book: Book):
     db.commit()
     # 新しい書籍情報をリフレッシュして、IDなどの自動生成フィールドを含める
     db.refresh(db_book)
-    # 新しい書籍オブジェクトを返す
-    return db_book
+    # 登録したbook_idを取得する
+    book_id=db_book.id
 
+    # 新しいMy_bookオブジェクトの作成
+    db_my_book = models.My_book(
+        user_id=user_id,
+        book_id=db_book.id,
+        genre_id=genre_id,
+        title=book.title,
+        author=book.author,
+        publisher=book.publisher,
+        total_page=book.total_page,
+        image=book.image,
+        start_date=book.start_date,
+        planned_end_date=book.planned_end_date,
+        end_date=None
+     )
+    # 新しい書籍をデータベースセッションに追加
+    db.add(db_my_book)
+    # 変更をコミットしてデータベースに保存
+    db.commit()
+    # 新しい書籍情報をリフレッシュして、IDなどの自動生成フィールドを含める
+    db.refresh(db_my_book)
+    # 新しい書籍オブジェクトを返す
+    return db_my_book
+
+
+# my_booksテーブルにレコードを追加(手動登録)
+def munual_create_my_book(db: Session, book: MyBook, user_id: int,genre_id: int):
+    # 新しいMy_bookオブジェクトの作成
+    db_my_book = models.My_book(
+        user_id=user_id,
+        book_id=None,
+        genre_id=genre_id,
+        title=book.title,
+        author=book.author,
+        publisher=book.publisher,
+        total_page=book.total_page,
+        image=book.image,
+        start_date=book.start_date,
+        planned_end_date=book.planned_end_date,
+        end_date=None
+     )
+    # 新しい書籍をデータベースセッションに追加
+    db.add(db_my_book)
+    # 変更をコミットしてデータベースに保存
+    db.commit()
+    # 新しい書籍情報をリフレッシュして、IDなどの自動生成フィールドを含める
+    db.refresh(db_my_book)
+    # 新しい書籍オブジェクトを返す
+    return db_my_book
 # reading_sessionsテーブルにレコードを追加
 def create_reading_session(db: Session, user_id: int, book_id: int, planned_end_date: date):
     # 新しいReadingSessionオブジェクトの作成
@@ -102,27 +184,16 @@ def create_reading_session(db: Session, user_id: int, book_id: int, planned_end_
 def get_genre_by_name(db: Session, name: str):
     return db.query(models.Genre).filter(models.Genre.name == name).first()
 
-# book_genreテーブルにレコードを追加
-def create_book_genre(db: Session, user_id: int, book_id: int, genre_id: int):
-    db_book_genre = models.Book_genre(
-        user_id=user_id,
-        book_id=book_id,
-        genre_id=genre_id,
-    )
-    db.add(db_book_genre)
-    db.commit()
-    db.refresh(db_book_genre)
-    return db_book_genre
 
 # tagsテーブルから引数に一致するタグを取得
-def get_tag_by_name(db: Session, name: str):
+def get_tag_by_name(db: Session, tag_name: str):
     # タグを取得するcrud処理
-    return db.query(models.Tag).filter(models.Tag.name == name).first()
+    return db.query(models.Tag).filter(models.Tag.tag_name == tag_name).first()
 
 # tagsテーブルにレコードを追加
-def create_tag(db: Session, name: str):
+def create_tag(db: Session, tag_name: str):
     # タグを登録するcrud処理
-    new_tag = models.Tag(name=name)
+    new_tag = models.Tag(tag_name=tag_name)
     # 新しいタグをデータベースセッションに追加
     db.add(new_tag)
     # 変更をコミットしてデータベースに保存
@@ -133,10 +204,9 @@ def create_tag(db: Session, name: str):
     return new_tag
 
 # book_tagsにレコードを追加
-def create_book_tag(db: Session, user_id: int, book_id: int, tag_id: int):
+def create_book_tag(db: Session, my_book_id: int, tag_id: int):
     new_book_tag = models.Book_tag(
-        user_id=user_id,
-        book_id=book_id,
+        my_book_id=my_book_id,
         tag_id=tag_id,
     )
     # 新しいbook_tagをデータベースセッションに追加
@@ -149,11 +219,10 @@ def create_book_tag(db: Session, user_id: int, book_id: int, tag_id: int):
     return new_book_tag
 
 # reading_sessionsからuser.idとbook.idを使用して書籍タイトルを取得
-def get_book_by_user_and_title(db: Session, user_id: int, title: str):
-    # `reading_sessions` と `books` を結合し、指定された `user_id` と `books.title` でフィルタリング
-    result = db.query(models.Book).\
-        join(models.Reading_session, models.Reading_session.book_id == models.Book.id).\
-        filter(models.Reading_session.user_id == user_id, models.Book.title == title).\
+def get_my_book_by_user_and_title(db: Session, user_id: int, title: str):
+    # ` `books` を `user_id` と `books.title` でフィルタリング
+    result = db.query(models.My_book).\
+        filter(models.My_book.user_id == user_id, models.My_book.title == title).\
         first()
 
     if result is None:
@@ -162,37 +231,23 @@ def get_book_by_user_and_title(db: Session, user_id: int, title: str):
     return result
 
 # daily_logsテーブルとbook_memoテーブルにレコードを追加
-def create_log(db: Session, user_id: int, book_id: int, page_read: int, reading_date: datetime, memo_text: str):
+def create_log(db: Session, my_book_id: int, page_read: int, reading_date: datetime, memo_text: str):
     # daily_logsをuser_idとbook_idでフィルタし、総ページ数を計算
     total_read_pages = db.query(func.sum(models.Daily_log.page_read)).filter(
-        models.Daily_log.user_id == user_id,
-        models.Daily_log.book_id == book_id
+        models.Daily_log.my_book_id == my_book_id
     ).scalar() or 0
-
+    
     page_read_today = page_read - total_read_pages
 
-    book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    book = db.query(models.My_book).filter(models.My_book.id == my_book_id).first()
     if book and page_read >= book.total_page:
-        reading_session = db.query(models.Reading_session).filter(
-            models.Reading_session.user_id == user_id,
-            models.Reading_session.book_id == book_id
-        ).first()
-        if reading_session:
-            reading_session.end_date = datetime.today()
-
+        book.end_date = datetime.today()
+    
     new_log = models.Daily_log(
-        user_id=user_id,
-        book_id=book_id,
+        my_book_id=my_book_id,
         page_read=page_read_today,
-        date=reading_date
-    )
-    db.add(new_log)
-
-    new_memo = models.Book_memo(
-        user_id=user_id,
-        book_id=book_id,
+        date=reading_date,
         memo=memo_text
     )
-    db.add(new_memo)
-
+    db.add(new_log)    
     db.commit()
