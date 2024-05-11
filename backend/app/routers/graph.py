ff7eeb25c31eea3cb9c183e2df0d573e75d9ad24
Miyuki Katada <happy.mk39@gmail.com>
2024-05-11 09:39:41 +0000

    create api/graph

---
+++ backend/app/routers/graph.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from datetime import datetime
+from ..database import get_db
+from app.calculation import (
+    calculate_pages_read_weekly,
+    calculate_pages_read_monthly,
+    calculate_pages_read_yearly,
+    calculate_genre_distribution_weekly,
+    calculate_genre_distribution_monthly,
+    calculate_genre_distribution_yearly
+    )
+
+router = APIRouter()
+
+@router.get("/")
+
+def get_reading_statistics(user_id: int, start_date: str, end_date: str, period: str, db: Session = Depends(get_db)):
+    try:
+        # 日付を適切に処理
+        start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+        end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+
+        # 棒グラフ用　ページ数の集計
+        if period == 'weekly':
+            pages_summary = calculate_pages_read_weekly(db, user_id, start_date, end_date)
+        elif period == 'monthly':
+            pages_summary = calculate_pages_read_monthly(db, user_id, start_date, end_date)
+        elif period == 'yearly':
+            pages_summary = calculate_pages_read_yearly(db, user_id, start_date, end_date)
+        else:
+            raise ValueError("Invalid period specified")
+
+        # 円グラフ用　ジャンル別の集計
+        if period == 'weekly':
+            genre_summary = calculate_genre_distribution_weekly(db, user_id, start_date, end_date)
+        elif period == 'monthly':
+            genre_summary = calculate_genre_distribution_monthly(db, user_id, start_date, end_date)
+        elif period == 'yearly':
+            genre_summary = calculate_genre_distribution_yearly(db, user_id, start_date, end_date)
+        else:
+            raise ValueError("Invalid period specified")
+
+        return {
+            "pages_summary": pages_summary,
+            "genre_summary": genre_summary
+        }
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
