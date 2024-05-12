f494d8e1895b998069cde7f3542b08cf02c3cb76
kunihiko <127746548+kunipico@users.noreply.github.com>
2024-05-12 15:36:35 +0900

    [Update]バーコードリーダーモーダルから書籍登録モーダルへ切り替え

--- frontend/app/src/components/barcodeReader.tsx
+++ frontend/app/src/components/barcodeReader.tsx
@@ -4,11 +4,14 @@ import Quagga from "@ericblade/quagga2";
 
 interface BarcodeReaderProps {
   onClose: () => void; // モーダルを閉じる関数
+  openForm: () => void; // BookRegisterForm を表示する関数
 }
 
 // export default function BarcodeReader() {
-export default function BarcodeReader({ onClose }: BarcodeReaderProps) {
-  // const [isOpen, setIsOpen] = useState(false);
+export default function BarcodeReader({
+  onClose,
+  openForm,
+}: BarcodeReaderProps) {
   const [image, setImage] = useState<string>("");
   const [barcode, setBarcode] = useState("");
 
@@ -42,27 +45,16 @@ export default function BarcodeReader({ onClose }: BarcodeReaderProps) {
 
   return (
     <>
-      {/* <Button
-        className="boder-0 m-4 rounded-xl bg-green-400 px-6 py-2 text-lg text-white duration-300 hover:bg-green-500"
-        onClick={() => setIsOpen(true)}
-      >
-        書籍登録
-      </Button> */}
-      {/* <Dialog
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-        className="relative z-50 "
-      > */}
       <Dialog open={true} onClose={onClose} className="relative z-50 ">
-        <div className="fixed inset-0 flex w-screen items-center justify-center bg-black/30 p-4">
-          <DialogPanel className="flex h-auto w-auto flex-col rounded-xl bg-cyan-100 backdrop:bg-gray-900">
-            <DialogTitle className="m-4 text-center text-lg text-gray-700 underline underline-offset-8">
+        <div className="fixed inset-0 flex w-screen items-center justify-center bg-black/30 p-4 text-gray-700">
+          <DialogPanel className="flex h-auto w-auto flex-col rounded-xl bg-cyan-100">
+            <DialogTitle className="m-4 text-center text-lg  underline underline-offset-8">
               書籍情報登録
             </DialogTitle>
-            <p className="mx-auto my-4 p-4 text-gray-700">
+            <p className="mx-auto my-4 p-4 ">
               本のバーコードを撮影してアップロードしてください
             </p>
-            <div className="m-2 flex flex-col text-gray-700">
+            <div className="m-2 flex flex-col">
               <input
                 className="m-4"
                 type="file"
@@ -76,13 +68,14 @@ export default function BarcodeReader({ onClose }: BarcodeReaderProps) {
               >
                 バーコードを読み取る
               </Button>
-              <Button className="boder-0 m-4 rounded-xl bg-cyan-400 px-6 py-2 text-lg text-white duration-300 hover:bg-cyan-500">
+              <Button
+                onClick={() => {
+                  openForm();
+                }}
+                className="boder-0 m-4 rounded-xl bg-cyan-400 px-6 py-2 text-lg text-white duration-300 hover:bg-cyan-500"
+              >
                 フォームから入力する
               </Button>
-              {/* <Button
-                className="boder-0 m-4 rounded-xl bg-cyan-400 px-6 py-2 text-lg text-white duration-300 hover:bg-cyan-500"
-                onClick={() => setIsOpen(false)}
-              > */}
               <Button
                 className="boder-0 m-4 rounded-xl bg-cyan-400 px-6 py-2 text-lg text-white duration-300 hover:bg-cyan-500"
                 onClick={onClose}
--- frontend/app/src/pages/modals.tsx
+++ frontend/app/src/pages/modals.tsx
@@ -4,20 +4,37 @@ import BookRegisterForm from "../components/bookRegisterForm";
 
 export default function Modals() {
   const [isBarcodeReaderOpen, setIsBarcodeReaderOpen] = useState(false);
+  const [isBookRegisterFormOpen, setIsBookRegisterFormOpen] = useState(false);
+
+  const openBarcodeReader = () => {
+    setIsBarcodeReaderOpen(true);
+  };
+
+  const openBookRegisterForm = () => {
+    console.log("HOGE");
+    setIsBarcodeReaderOpen(false); // BarcodeReader モーダルを閉じる
+    setIsBookRegisterFormOpen(true); // BookRegisterForm モーダルを開く
+  };
 
   return (
     <>
       <button
         className="boder-0 m-4 rounded-xl bg-green-400 px-6 py-2 text-lg text-white duration-300 hover:bg-green-500"
-        onClick={() => setIsBarcodeReaderOpen(true)} // ボタンがクリックされた時にモーダルを開く
+        onClick={openBarcodeReader} // ボタンがクリックされた時にモーダルを開く
       >
         書籍登録
       </button>
-      {isBarcodeReaderOpen && (
-        <BarcodeReader onClose={() => setIsBarcodeReaderOpen(false)} />
-      )}{" "}
       {/* isBarcodeReaderOpen が true の時に BarcodeReader を表示 */}
-      <BookRegisterForm />
+      {isBarcodeReaderOpen && (
+        <BarcodeReader
+          onClose={() => setIsBarcodeReaderOpen(false)}
+          openForm={openBookRegisterForm}
+        />
+      )}{" "}
+      {/* isBookRegisterFormOpen が true の時に BookRegisterForm を表示 */}
+      {isBookRegisterFormOpen && (
+        <BookRegisterForm onClose={() => setIsBookRegisterFormOpen(false)} />
+      )}{" "}
     </>
   );
 }
