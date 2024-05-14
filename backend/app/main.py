9f6190918b507b609bb73d50081216f9cd836f1c
羽田　有希 <naoki@hatayukinoMacBook-Pro.local>
2024-05-14 23:32:05 +0900

    main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,8 +15,7 @@ from .routers.get_my_books_title import router as get_my_books_title
 from .routers.get_library import router as get_library
 from .routers.graph import router as graph
 from .routers.awards import router as awards
-from .routers.get_book_detail import router as get_my_book_detail
-from .routers.patch_book_detail import router as patch_book_detail
+
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -32,5 +31,4 @@ app.include_router(get_my_books_title, prefix="/api/my_books_title", tags=["get_
 app.include_router(get_library, prefix="/api/library", tags=["get_library"])
 app.include_router(graph, prefix="/api/graph", tags=["graph"])
 app.include_router(awards, prefix="/api/awards", tags=["awards"])
-app.include_router(get_my_book_detail, prefix="/api/book_detail", tags=["get_my_book_detail"])
-app.include_router(patch_book_detail, prefix="/api/book_detail", tags=["patch_book_detail"])
+
